d2c7b61ec13eaf0023b901ccf3e8ef0173797cd7
Rafał Więcek <gitzzz14@gmail.com>
2024-05-17 23:38:19 +0200

    Introduced benchmarking for requests
    A new file, bench-request.js, has been added to perform benchmarking on HTTP requests using the 'undici' library. The package.json scripts have been updated to include commands for running these benchmarks. This allows us to compare performance between different methods of making HTTP requests.

---
+++ bench-request.js
@@ -0,0 +1,8 @@
+import { request } from 'undici'
+
+console.time('fetch')
+for (let i = 0; i < 100000; i++) {
+  const req = await request('http://localhost:3000')
+  await req.text()
+}
+console.timeEnd('fetch')
